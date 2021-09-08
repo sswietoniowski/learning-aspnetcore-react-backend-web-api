7cd73d770962f65cd2bcecd566af062d8e83a380
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-09-08 16:24:37 +0200

    Extended GetQuestions action

--- Controllers/QuestionsController.cs
+++ Controllers/QuestionsController.cs
@@ -21,10 +21,16 @@ namespace learning_aspnetcore_react_backend_web_api.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<QuestionGetManyResponse> GetQuestions()
+        public IEnumerable<QuestionGetManyResponse> GetQuestions(string search)
         {
-            var questions = _dataRepository.GetQuestions();
-            return questions;
+            if (string.IsNullOrEmpty(search))
+            {
+                return _dataRepository.GetQuestions();
+            }
+            else
+            {
+                return _dataRepository.GetQuestionsBySearch(search);
+            }
         }
     }
 }
