3d6610e9a0e994752f1f8ad86a71e1fc595ff17f
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-09-08 16:42:29 +0200

    Added validation to posting answer

--- Models/AnswerPostRequest.cs
+++ Models/AnswerPostRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace learning_aspnetcore_react_backend_web_api.Models
 {
     public class AnswerPostRequest
     {
-        public int QuestionId { get; set; }
+        [Required]
+        public int? QuestionId { get; set; }
+        [Required]
         public string Content { get; set; }
         public string UserId { get; set; }
         public string UserName { get; set; }
