d4ca184b9aaa6cc305b52cf53d6de206a419708b
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-09-08 16:19:03 +0200

    Injected data repository into controller

---
+++ Controllers/QuestionsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using learning_aspnetcore_react_backend_web_api.Data;
+using learning_aspnetcore_react_backend_web_api.Models;
+
+namespace learning_aspnetcore_react_backend_web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly IDataRepository _dataRepository;
+
+        public QuestionsController(IDataRepository dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DbUp;
+using learning_aspnetcore_react_backend_web_api.Data;
 
 namespace learning_aspnetcore_react_backend_web_api
 {
@@ -43,6 +44,7 @@ namespace learning_aspnetcore_react_backend_web_api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "learning_aspnetcore_react_backend_web_api", Version = "v1" });
             });
+            services.AddScoped<IDataRepository, DataRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
