d1b8c5fa27ff353c0c1a161d104440f128de77ff
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-09-08 16:40:07 +0200

    Added validation to updating question

--- Models/QuestionPutRequest.cs
+++ Models/QuestionPutRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace learning_aspnetcore_react_backend_web_api.Models
 {
     public class QuestionPutRequest
     {
+        [StringLength(100)]
         public string Title { get; set; }
         public string Content { get; set; }
     }
