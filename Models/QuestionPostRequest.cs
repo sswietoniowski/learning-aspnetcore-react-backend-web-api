f1631f9211536731caf93ab1da4a714f0dd037b0
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-09-08 16:39:28 +0200

    Added validation to posting question

--- Models/QuestionPostRequest.cs
+++ Models/QuestionPostRequest.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace learning_aspnetcore_react_backend_web_api.Models
 {
     public class QuestionPostRequest
     {
+        [Required]
+        [StringLength(100)]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Please include some content for the question")]
         public string Content { get; set; }
         public string UserId { get; set; }
         public string UserName { get; set; }
